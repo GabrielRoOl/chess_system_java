38189cdaf0fd0671e4fad3a8599011a616388552
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-14 12:21:41 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.IllformedLocaleException;
 import java.util.List;
@@ -128,7 +127,7 @@ public class ChessMatch {
 			throw new IllformedLocaleException("There is no piece to be promoted.");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion.");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
